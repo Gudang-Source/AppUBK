e96d1e9f175b061c87313a5fa28f5d8a1ac37ffb
firnandahanif <40018817+firnandahanif@users.noreply.github.com>
2019-02-24 06:08:28 +0800

    tampilan awal halaman admin

--- admin/application/views/module/guru/guru.php
+++ admin/application/views/module/guru/guru.php
@@ -1,1 +1,14 @@
-<h1>SAYA MENU GURU</h1>
+<table class="table table-bordered">
+    <thead>
+        <th scope="col">No</th>
+        <th scope="col">Nama</th>
+        <th scope="col">NIP</th>
+    </thead>
+    <tbody>
+        <tr>
+            <th scope="row">1</th>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+    </tbody>
