52d2f3e7f5656f29a2b05b955ed136d95e6b729d
sfends <bintangfadel73@gmail.com>
2019-02-23 12:41:27 +0800

    tampilan

--- application/views/module/home/home.php
+++ application/views/module/home/home.php
@@ -1,51 +1,55 @@
-<div class="row">
-    <div class="col-md-4">
-    <div class="card card-default">
-            <img src="" class="card-img-top rounded mx-auto d-block" alt="...">
-            <div class="card-body">
-                <table class="table">
-                    <tbody>
-                        <tr>
-                            <td class="font-weight-bold">Nama :</td>
-                            <td><?php echo $siswa->nama; ?></td>
-                        </tr>
-                        <tr>
-                            <td class="font-weight-bold">Kelas :</td>
-                            <td><?php echo $ujian->nama_kelas; ?></td>
-                        </tr>
-                    </tbody>
-                </table>
+<div class="container" style="margin-top: 10px;">
+    <div class="row">
+        <div class="col-12 col-sm-4 col-lg-6">
+            <div class="card card-default">
+                <div class="card-header">
+                    <img src="" class="card-img-top rounded mx-auto d-block" alt="...">
+                </div>
+                <div class="card-body">
+                    <table class="table">
+                        <tbody>
+                            <tr>
+                                <td class="font-weight-bold">Nama</td>
+                                <td><?php echo $siswa->nama; ?></td>
+                            </tr>
+                            <tr>
+                                <td class="font-weight-bold">Kelas</td>
+                                <td><?php echo $ujian->nama_kelas; ?></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="col-md-4">
-        <div class="card card-default">
-            <div class="card-header">
-                Info Ujian
-            </div>
-            <div class="card-body">
-                <table class="table">
-                    <tbody>
-                        <tr>
-                            <td>Mata Pelajaran</td>
-                            <td><?php echo $ujian->nama; ?></td>
-                        </tr>
-                        <tr>
-                            <td>KKM</td>
-                            <td><?php echo $ujian->KKM; ?></td>
-                        </tr>
-                    </tbody>
-                </table>
+        <div class="col-12 col-sm-4 col-lg-6">
+            <div class="card card-default">
+                <div class="card-header">
+                    Info Ujian
+                </div>
+                <div class="card-body">
+                    <table class="table">
+                        <tbody>
+                            <tr>
+                                <td>Mata Pelajaran</td>
+                                <td><?php echo $ujian->nama; ?></td>
+                            </tr>
+                            <tr>
+                                <td>KKM</td>
+                                <td><?php echo $ujian->KKM; ?></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="col-md-4">
-        <div class="alert alert-info" style="background-color:#00c0ef !important;color:white;padding:30px;">
-            <h4>
-                <i class="icon fa fa-info"></i> Perhatian!
-            </h4>
-            Silahkan klik tombol mulai ujian dibawah ini, jika anda telah siap melaksanakan ujian.
+        <div class="col-12 col-sm-4">
+            <div class="alert alert-info" style="background-color:#00c0ef !important;color:white;padding:30px;">
+                <h4>
+                    <i class="icon fa fa-info"></i> Perhatian!
+                </h4>
+                Silahkan klik tombol mulai ujian dibawah ini, jika anda telah siap melaksanakan ujian.
+            </div>
+            <a class="btn btn-success btn-lg btn-block" href="<?=base_url('welcome/soal')?>">Mulai Ujian</a>
         </div>
-        <a class="btn btn-success btn-lg btn-block" href="<?=base_url('welcome/soal')?>">Mulai Ujian</a>
     </div>
 </div>
--- application/views/module/soal/soal.php
+++ application/views/module/soal/soal.php
@@ -1,35 +1,37 @@
-<div class="row">
-    <?php var_dump($soal); ?>
-	<div class="col-md-12">
-		<div class="card">
-			<div class="card-header">
-                Selamat Ujian Muhammad Syarif Hidayatullah
-			</div>
-			<div class="card-body">
-                <?php $no=1 ?>
-				<?php foreach ($soal as $data) {
-                    echo $no++.'.'.$data->soal_deskripsi; ?> <br>
-                    <input type="radio" name="pilgan" value="<?php echo $data->soal_jwb1 ?>"> <?php echo $data->soal_jwb1 ?><br>
-                    <input type="radio" name="pilgan" value="<?php echo $data->soal_jwb2 ?>"> <?php echo $data->soal_jwb2 ?><br>
-                    <input type="radio" name="pilgan" value="<?php echo $data->soal_jwb3 ?>"> <?php echo $data->soal_jwb3 ?><br>
-                    <input type="radio" name="pilgan" value="<?php echo $data->soal_jwb4 ?>"> <?php echo $data->soal_jwb4 ?><br>
-                    <input type="radio" name="pilgan" value="<?php echo $data->soal_jwb5 ?>"> <?php echo $data->soal_jwb5 ?>
-                <?php } ?>
-            </div>
-            <div class="card-footer">
-                <div class="row">
-                    <div class="col-6"><?php echo $this->pagination->create_links(); ?></div>
-                    <div class="col-md-6">
-                        <a href="" class="w-100 btn btn-info">Prev</a>    
-                    </div>
-                    <!-- <div class="col-md-4">
-                        <a href="" class="w-100 btn btn-success">Selesai</a>
-                    </div> -->
-                    <div class="col-md-6">
-                        <a href="" class="w-100 btn btn-primary">Next</a>
+<div class="container">
+    <div class="row">
+        <?php var_dump($soal); ?>
+    	<div class="col-md-12">
+    		<div class="card">
+    			<div class="card-header">
+                    Selamat Ujian Muhammad Syarif Hidayatullah
+    			</div>
+    			<div class="card-body">
+                    <?php $no=1 ?>
+    				<?php foreach ($soal as $data) {
+                        echo $no++.'.'.$data->soal_deskripsi; ?> <br>
+                        <input type="radio" name="pilgan" value="<?php echo $data->soal_jwb1 ?>"> <?php echo $data->soal_jwb1 ?><br>
+                        <input type="radio" name="pilgan" value="<?php echo $data->soal_jwb2 ?>"> <?php echo $data->soal_jwb2 ?><br>
+                        <input type="radio" name="pilgan" value="<?php echo $data->soal_jwb3 ?>"> <?php echo $data->soal_jwb3 ?><br>
+                        <input type="radio" name="pilgan" value="<?php echo $data->soal_jwb4 ?>"> <?php echo $data->soal_jwb4 ?><br>
+                        <input type="radio" name="pilgan" value="<?php echo $data->soal_jwb5 ?>"> <?php echo $data->soal_jwb5 ?>
+                    <?php } ?>
+                </div>
+                <div class="card-footer">
+                    <div class="row">
+                        <div class="col-12"><?php echo $this->pagination->create_links(); ?></div>
+                        <div class="col-md-6">
+                            <a href="" class="w-100 btn btn-info">Prev</a>    
+                        </div>
+                        <!-- <div class="col-md-4">
+                            <a href="" class="w-100 btn btn-success">Selesai</a>
+                        </div> -->
+                        <div class="col-md-6">
+                            <a href="" class="w-100 btn btn-primary">Next</a>
+                        </div>
                     </div>
                 </div>
-            </div>
-	    </div>
-	</div>
+    	    </div>
+    	</div>
+    </div>
 </div>
