996e34ac6e77374bc717164f059c5a6931331f34
Hendri Arifin, S.Kom <cowok_cool320@yahoo.co.id>
2019-03-09 15:58:07 +0800

    change template

--- admin/application/views/module/perSoal/perSoal.php
+++ admin/application/views/module/perSoal/perSoal.php
@@ -1,98 +1,109 @@
-<h1>SOAL DAN JAWABAN</h1>
-<div class="col-lg-12">
-    <div class="card">
-        <div class="card-body card-block">
-            <form action="" class="form" method="post" enctype="multipart/form-data">
-				<div class="form-group">
-					<label>Isi Soal</label>
-					<textarea name="soal_deskripsi" class="form-control" cols="50" rows="10"></textarea>
-				</div>
-				<label>Jawaban</label><br>
-				<small class="text-muted">*Isi jawaban dan pilih salah satu jawaban yang benar</small>
-				<div class="input-group input-group-sm mb-3">
-					<div class="input-group-prepend">
-						<span class="input-group-text">A</span>
-						<div class="input-group-text">
-							<input value="soal_jwb1" required type="radio" name="soal_jawaban">
-						</div>
-					</div>
-					<textarea name="soal_jwb1" class="form-control"></textarea>
-				</div>
-				<div class="input-group mb-3">
-					<div class="input-group-prepend">
-						<span class="input-group-text">B</span>
-						<div class="input-group-text">
-							<input value="soal_jwb2" type="radio" name="soal_jawaban">
-						</div>
-					</div>
-					<textarea name="soal_jwb2" class="form-control"></textarea>
-					<!-- <input name="soal_jwb2" required type="text" class="form-control" placeholder="Jawaban B"> -->
-				</div>
-				<div class="input-group mb-3">
-					<div class="input-group-prepend">
-						<span class="input-group-text">C</span>
-						<div class="input-group-text">
-							<input value="soal_jwb3" type="radio" name="soal_jawaban">
-						</div>
-					</div>
-					<textarea name="soal_jwb3" class="form-control"></textarea>
-					<!-- <input name="soal_jwb3" required type="text" class="form-control" placeholder="Jawaban C"> -->
-				</div>
-				<div class="input-group mb-3">
-					<div class="input-group-prepend">
-						<span class="input-group-text">D</span>
-						<div class="input-group-text">
-							<input value="soal_jwb4" type="radio" name="soal_jawaban">
-						</div>
-					</div>
-					<textarea name="soal_jwb4" class="form-control"></textarea>
-					<!-- <input name="soal_jwb4" required type="text" class="form-control" placeholder="Jawaban D"> -->
-				</div>
-				<div class="input-group mb-3">
-					<div class="input-group-prepend">
-						<span class="input-group-text">E</span>
-						<div class="input-group-text">
-							<input value="soal_jwb5" type="radio" name="soal_jawaban">
-						</div>
-					</div>
-					<textarea name="soal_jwb5" class="form-control"></textarea>
-					<!-- <input name="soal_jwb5" required type="text" class="form-control" placeholder="Jawaban E"> -->
-				</div>	
-				<input type="submit" name="submit" class="btn btn-success">
-			</form>
-        </div>
-    </div>
-</div>
-<?php $no=1; foreach($perSoal AS $data) { ?>
-	<div class="card-body">
-		<hr>
-		<div class="float-right">
-			<a href="<?php echo base_url("welcome/perSoalEdit/".$data->soal_id.'/'.$data->soal_pelajaran) ?>" class="btn btn-info" >Edit</a>
+<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+	<h1 class="h2">Create Soal</h1>
+	<div class="btn-toolbar mb-2 mb-md-0">
+		<div class="btn-group mr-2">
+			<button type="button" class="btn btn-sm btn-outline-secondary">Share</button>
+			<button type="button" class="btn btn-sm btn-outline-secondary">Export</button>
 		</div>
-		<?php echo "Soal No. ".$no; ?>
-		<?php echo $data->soal_deskripsi."</br>" ?>
-		<form>
-			<label class="jawaban w-50"><?php echo $data->soal_jwb1 ?>
-    		    <input @change="updateJawaban(<?php echo $data->soal_id ?>,'<?php echo $data->soal_jwb1 ?>')" <?php if($data->soal_jawaban==$data->soal_jwb1){echo "checked";} ?> type="radio" name="pilgan" >
-    		    <span class="checkmark">A</span>
-    		</label>
-    		<label class="jawaban w-50"><?php echo $data->soal_jwb2 ?>
-    		    <input @change="updateJawaban(<?php echo $data->soal_id ?>,'<?php echo $data->soal_jwb2 ?>')" <?php if($data->soal_jawaban==$data->soal_jwb2){echo "checked";} ?> type="radio" name="pilgan" >
-    		    <span class="checkmark">B</span>
-    		</label>
-    		<label class="jawaban w-50"><?php echo $data->soal_jwb3 ?>
-    		    <input @change="updateJawaban(<?php echo $data->soal_id ?>,'<?php echo $data->soal_jwb3 ?>')" <?php if($data->soal_jawaban==$data->soal_jwb3){echo "checked";} ?> type="radio" name="pilgan" >
-    		    <span class="checkmark">C</span>
-    		</label>
-    		<label class="jawaban w-50"><?php echo $data->soal_jwb4 ?>
-    		    <input @change="updateJawaban(<?php echo $data->soal_id ?>,'<?php echo $data->soal_jwb4 ?>')" <?php if($data->soal_jawaban==$data->soal_jwb4){echo "checked";} ?> type="radio" name="pilgan" >
-    		    <span class="checkmark">D</span>
-    		</label>
-    		<label class="jawaban w-50"><?php echo $data->soal_jwb5 ?>
-    		    <input @change="updateJawaban(<?php echo $data->soal_id ?>,'<?php echo $data->soal_jwb5 ?>')" <?php if($data->soal_jawaban==$data->soal_jwb5){echo "checked";} ?> type="radio" name="pilgan" >
-    		    <span class="checkmark">E</span>
-    		</label>
-		</form>
-		<hr>
+		<button type="button" class="btn btn-sm btn-outline-secondary dropdown-toggle">
+			<span data-feather="calendar"></span>
+			This week
+		</button>
+	</div>
+</div>
+<!-- Form Soal -->
+<form action="" class="form mb-5" method="post" enctype="multipart/form-data">
+	<div class="form-group">
+		<label>Isi Soal</label>
+		<textarea name="soal_deskripsi" class="form-control" cols="50" rows="10"></textarea>
+	</div>
+	<label>Jawaban</label><br>
+	<small class="text-muted">*Isi jawaban dan pilih salah satu jawaban yang benar</small>
+	<div class="input-group input-group-sm mb-3">
+		<div class="input-group-prepend">
+			<span class="input-group-text">A</span>
+			<div class="input-group-text">
+				<input value="soal_jwb1" required type="radio" name="soal_jawaban">
+			</div>
+		</div>
+		<textarea name="soal_jwb1" class="form-control"></textarea>
+	</div>
+	<div class="input-group mb-3">
+		<div class="input-group-prepend">
+			<span class="input-group-text">B</span>
+			<div class="input-group-text">
+				<input value="soal_jwb2" type="radio" name="soal_jawaban">
+			</div>
+		</div>
+		<textarea name="soal_jwb2" class="form-control"></textarea>
+		<!-- <input name="soal_jwb2" required type="text" class="form-control" placeholder="Jawaban B"> -->
+	</div>
+	<div class="input-group mb-3">
+		<div class="input-group-prepend">
+			<span class="input-group-text">C</span>
+			<div class="input-group-text">
+				<input value="soal_jwb3" type="radio" name="soal_jawaban">
+			</div>
+		</div>
+		<textarea name="soal_jwb3" class="form-control"></textarea>
+		<!-- <input name="soal_jwb3" required type="text" class="form-control" placeholder="Jawaban C"> -->
+	</div>
+	<div class="input-group mb-3">
+		<div class="input-group-prepend">
+			<span class="input-group-text">D</span>
+			<div class="input-group-text">
+				<input value="soal_jwb4" type="radio" name="soal_jawaban">
+			</div>
+		</div>
+		<textarea name="soal_jwb4" class="form-control"></textarea>
+		<!-- <input name="soal_jwb4" required type="text" class="form-control" placeholder="Jawaban D"> -->
+	</div>
+	<div class="input-group mb-3">
+		<div class="input-group-prepend">
+			<span class="input-group-text">E</span>
+			<div class="input-group-text">
+				<input value="soal_jwb5" type="radio" name="soal_jawaban">
+			</div>
+		</div>
+		<textarea name="soal_jwb5" class="form-control"></textarea>
+		<!-- <input name="soal_jwb5" required type="text" class="form-control" placeholder="Jawaban E"> -->
+	</div>	
+	<input type="submit" name="submit" class="btn btn-success">
+</form>
+
+<!-- Daftar List Soal -->
+<?php $no=1; foreach($perSoal AS $data) { ?>
+	<div class="card bg-light">
+		<div class="card-header">
+			<?php echo "<b>Soal No. ".$no."</b>" ?>
+			<div class="float-right">
+				<a href="<?php echo base_url("welcome/perSoalEdit/".$data->soal_id.'/'.$data->soal_pelajaran) ?>" class="btn btn-info btn-sm" >Edit</a>
+			</div>
+		</div>
+		<div class="card-body">
+			<?php echo $data->soal_deskripsi."</br>" ?>
+			<form>
+				<label class="jawaban w-50"><?php echo $data->soal_jwb1 ?>
+					<input @change="updateJawaban(<?php echo $data->soal_id ?>,'<?php echo $data->soal_jwb1 ?>')" <?php if($data->soal_jawaban==$data->soal_jwb1){echo "checked";} ?> type="radio" name="pilgan" >
+					<span class="checkmark">A</span>
+				</label>
+				<label class="jawaban w-50"><?php echo $data->soal_jwb2 ?>
+					<input @change="updateJawaban(<?php echo $data->soal_id ?>,'<?php echo $data->soal_jwb2 ?>')" <?php if($data->soal_jawaban==$data->soal_jwb2){echo "checked";} ?> type="radio" name="pilgan" >
+					<span class="checkmark">B</span>
+				</label>
+				<label class="jawaban w-50"><?php echo $data->soal_jwb3 ?>
+					<input @change="updateJawaban(<?php echo $data->soal_id ?>,'<?php echo $data->soal_jwb3 ?>')" <?php if($data->soal_jawaban==$data->soal_jwb3){echo "checked";} ?> type="radio" name="pilgan" >
+					<span class="checkmark">C</span>
+				</label>
+				<label class="jawaban w-50"><?php echo $data->soal_jwb4 ?>
+					<input @change="updateJawaban(<?php echo $data->soal_id ?>,'<?php echo $data->soal_jwb4 ?>')" <?php if($data->soal_jawaban==$data->soal_jwb4){echo "checked";} ?> type="radio" name="pilgan" >
+					<span class="checkmark">D</span>
+				</label>
+				<label class="jawaban w-50"><?php echo $data->soal_jwb5 ?>
+					<input @change="updateJawaban(<?php echo $data->soal_id ?>,'<?php echo $data->soal_jwb5 ?>')" <?php if($data->soal_jawaban==$data->soal_jwb5){echo "checked";} ?> type="radio" name="pilgan" >
+					<span class="checkmark">E</span>
+				</label>
+			</form>
+		</div>
 	</div>
 <?php $no++; } ?>
--- admin/application/views/welcome_message.php
+++ admin/application/views/welcome_message.php
@@ -5,98 +5,75 @@
 	<meta charset="utf-8">
 	<title>Admin - Aplikasi USBN SMKN 1 Kotabaru</title>
     <link rel="stylesheet" href="<?php echo base_url('../assets/css/bootstrap.min.css'); ?>">
-    <!-- <link rel="stylesheet" href="assets/css/cs-skin-elastic.css"> -->
+    <link rel="stylesheet" href="<?php echo base_url('../assets/css/dashboard.css'); ?>">
     <link rel="stylesheet" href="<?php echo base_url('./assets/css/style.css'); ?>">
-    <!-- <script type="text/javascript" src="https://cdn.jsdelivr.net/html5shiv/3.7.3/html5shiv.min.js"></script> -->
-    <!-- <link href="https://cdn.jsdelivr.net/npm/chartist@0.11.0/dist/chartist.min.css" rel="stylesheet"> -->
-    <!-- <link href="https://cdn.jsdelivr.net/npm/jqvmap@1.5.1/dist/jqvmap.min.css" rel="stylesheet"> -->
-
-    <!-- <link href="https://cdn.jsdelivr.net/npm/weathericons@2.1.0/css/weather-icons.css" rel="stylesheet" /> -->
-    <!-- <link href="https://cdn.jsdelivr.net/npm/fullcalendar@3.9.0/dist/fullcalendar.min.css" rel="stylesheet" /> -->
 </head>
 <body>
-<div id="appAdmin" v-cloak>
-    <aside id="left-panel" class="left-panel">
-        <nav class="navbar navbar-expand-sm navbar-default">
-            <div id="main-menu" class="main-menu collapse navbar-collapse">
-                <ul class="nav navbar-nav">
-                    <li class="menu-title">
-                        <a href="<?php echo base_url('welcome/guru')?>"><i class="fa fa-user"></i> Menu guru</a>
-                    </li>
-                    <li class="menu-title">
-                        <a href="<?php echo base_url('welcome/kelas')?>"><i class="fa fa-users"></i> Menu kelas</a>
-                    </li>
-                    <li class="menu-title">
-                        <a href="<?php echo base_url('welcome/siswa')?>"><i class="fa fa-users"></i> Menu Siswa</a>
-					</li>
-                    <li class="menu-title">
-                        <a href="<?php echo base_url('welcome/soal')?>"><i class="fa fa-users"></i> Menu soal</a>
-					</li>
-                    <li class="menu-title">
-                        <a href="<?php echo base_url('welcome/ujian')?>"><i class="fa fa-users"></i> Menu ujian</a>
-					</li>
-                </ul>
-            </div><!-- /.navbar-collapse -->
-        </nav>
-    </aside>
-    <!-- /#left-panel -->
-    <!-- Right Panel -->
-    <div id="right-panel" class="right-panel">
-        <!-- Header-->
-        <header id="header" class="header">
-			<div class="top-left">
-                <div class="navbar-header">
-					<a href="">AppUBK</a>
-					<a id="menuToggle" class="menutoggle"><i class="fa fa-bars"></i></a>
+    <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+        <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">
+            Aplikasi UNBK
+            <div class="spinner-border text-primary float-right" role="status"></div>
+        </a>
+		<input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
+		<ul class="navbar-nav px-3">
+			<li class="nav-item text-nowrap">
+				<a class="nav-link" href="#">Sign out</a>
+			</li>
+		</ul>
+	</nav>
+    <div class="container-fluid">
+        <div class="row">
+            <nav class="col-md-2 d-none d-md-block bg-light sidebar">
+				<div class="sidebar-sticky">
+					<ul class="nav flex-column">
+						<li class="nav-item">
+							<a class="nav-link active" href="<?php echo base_url('welcome/guru')?>">
+								<span data-feather="home"></span>
+								Menu Guru <span class="sr-only">(current)</span>
+							</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="<?php echo base_url('welcome/kelas')?>">
+								<span data-feather="file"></span>
+								Menu Kelas
+							</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="<?php echo base_url('welcome/siswa')?>">
+								<span data-feather="shopping-cart"></span>
+								Menu Siswa
+							</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="<?php echo base_url('welcome/soal')?>">
+								<span data-feather="shopping-cart"></span>
+								Menu Soal
+							</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="<?php echo base_url('welcome/ujian')?>">
+								<span data-feather="shopping-cart"></span>
+								Menu Ujian
+							</a>
+						</li>
+                    </ul>
                 </div>
-            </div>
-            <div class="top-right">
-                <div class="header-menu">
-                    <div class="header-left">
-                        <button class="search-trigger"><i class="fa fa-search"></i></button>
-                        <div class="form-inline">
-                            <form class="search-form">
-                                <input class="form-control " type="text" placeholder="Search ..." aria-label="Search">
-                                <button class="search-close" type="submit"><i class="fa fa-close"></i></button>
-                            </form>
+            </nav>
+            <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4" id="appAdmin" v-cloak>
+                <?php echo $content; ?>
+                <div class="card-footer">
+                    <div class="row">
+                        <div class="col-6">
+                            Powered By <strong>Hendri Arifin, S.Kom - 085746080544</strong>
                         </div>
-                        <a href="#sign-out"><i class="fa fa-sign-out"></i></a>
+                        <div class="col-6 text-right">
+                            Page rendered in <strong>{elapsed_time}</strong> seconds
+                        </div>
                     </div>
-
-
                 </div>
-            </div>
-        </header>
-        <div class="content">
-                <div class="row">
-                        <div class="col-lg-12 col-md-12">
-                            <div class="card">
-                                <div class="card-body">
-                                    <div class="">
-                                        <div class="text-left dib col-lg-12 col-md-12">
-											<?php echo $content; ?>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                </div>
+            </main>
         </div>
-        <!-- /.content -->
-        <!-- /#right-panel -->
-        <div class="clearfix"></div>
-        <!-- Footer -->
-		<footer class="site-footer">
-            <div class="footer-inner bg-white">
-                <div class="row">
-					<div class="col-xs-6">
-                        Copyright &copy; 2019 Computer Media Utama 
-					</div>
-                </div>
-            </div>
-        </footer>
     </div>
-</div>
     <script src="<?php echo base_url('../assets/js/vue.js'); ?>"></script>
     <script src="<?php echo base_url('../assets/js/axios.min.js'); ?>"></script>
     <script src="<?php echo base_url('./assets/js/nicEdit.js'); ?>"></script> 
@@ -104,8 +81,13 @@
     <script type="text/javascript"> 
         bkLib.onDomLoaded(function() { 
             nicEditors.allTextAreas({iconsPath:'<?php echo base_url(); ?>assets/js/nicEditorIcons.gif',uploadURI:'<?php echo site_url() ?>./welcome/upload'});
-            // nicEditors.allTextAreas();
         });
     </script>
+    
+	<!-- icon -->
+	<script src="https://unpkg.com/feather-icons/dist/feather.min.js"></script>
+	<script>
+		feather.replace()
+	</script>
 </body>
 </html>
